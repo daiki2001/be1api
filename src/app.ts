b5b6650220fea94d5c921c7a866860ec8c02ddc9
daiki2001 <daiki20011222@gmail.com>
2023-08-02 11:29:53 +0900

    POST,PATCH,DELETEの追加

--- src/app.ts
+++ src/app.ts
@@ -7,16 +7,19 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.listen(config.port, () => {
- console.log(`Start on port ${config.port}.`);
+  console.log(`Start on port ${config.port}.`);
 });
 
 const connection = async () => {
- return await mysql.createConnection(config.db);
+  return await mysql.createConnection(config.db);
 };
+
+app.get("/", (req, res) => {
+  res.send('Hello World!');
+});
 
 // faculty一覧取得
 app.get("/faculty", (req: express.Request, res: express.Response) => {
-  const facultyId = req.params.facultyId;
   connection()
     .then((connection) => {
       const result = connection.query("SELECT * FROM FACULTY");
@@ -26,20 +29,70 @@ app.get("/faculty", (req: express.Request, res: express.Response) => {
     .then(function(rows) {
       res.send(rows);
     });
- });
+});
  
 // faculty単一取得
 app.get("/faculty/:facultyId", (req: express.Request, res: express.Response) => {
- const facultyId = req.params.facultyId;
- connection()
-   .then((connection) => {
-     const result = connection.query("SELECT * FROM FACULTY WHERE ID = ?", [
-       facultyId,
-     ]);
-     connection.end();
-     return result;
-   })
-   .then(function(rows) {
-     res.send(rows);
-   });
+  const facultyId = req.params.facultyId;
+  connection()
+    .then((connection) => {
+      const result = connection.query("SELECT * FROM FACULTY WHERE ID = ?", [
+        facultyId,
+      ]);
+      connection.end();
+      return result;
+    })
+    .then(function(rows) {
+      res.send(rows);
+    });
+});
+
+// faculty追加処理
+app.post("/faculty", (req: express.Request, res: express.Response) => {
+  const name = req.body.name;
+  connection()
+    .then((connection) => {
+      const result = connection.query("INSERT INTO FACULTY (NAME) VALUES (?)", [
+        name,
+      ]);
+      connection.end();
+      return result;
+    })
+    .then(function(rows) {
+      res.send(rows);
+    });
+});
+
+// faculty更新処理
+app.patch("/faculty/:facultyId", (req: express.Request, res: express.Response) => {
+  const facultyId = req.params.facultyId;
+  const name = req.body.name;
+  connection()
+    .then((connection) => {
+      const result = connection.query("UPDATE FACULTY SET NAME = ? WHERE ID = ?", [
+        name,
+        facultyId,
+      ]);
+      connection.end();
+      return result;
+    })
+    .then(function(rows) {
+      res.send(rows);
+    });
+});
+
+// faculty削除処理
+app.delete("/faculty/:facultyId", (req: express.Request, res: express.Response) => {
+  const facultyId = req.params.facultyId;
+  connection()
+    .then((connection) => {
+      const result = connection.query("DELETE FROM FACULTY WHERE ID = ?", [
+        facultyId,
+      ]);
+      connection.end();
+      return result;
+    })
+    .then(function(rows) {
+      res.send(rows);
+    });
 });
